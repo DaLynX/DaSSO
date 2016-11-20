9f9db7751ce2b4ddbd7f6935188c09158289e017
DaLynX <d@l.ynx.fr>
2016-11-20 01:19:48 +0000

    Added 401 in case user is not authenticated and ressource is not accessible to AnonymousUser

--- src/sso/dasso/views.py
+++ src/sso/dasso/views.py
@@ -8,5 +8,7 @@ def check_user_access(request, target_vhost):
     target_ressource = Ressource.objects.get(vhost=target_vhost)
     if request.user.has_perm('access_ressource', target_ressource):
         return HttpResponse()
+    elif request.user.is_anonymous:
+        return HttpResponse(status=401)
     else:
         return HttpResponse(status=403)
