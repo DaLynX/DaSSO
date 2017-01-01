c568dec4d87d95ce5599a56a6bce9efba5d9d4d2
DaLynX <d@l.ynx.fr>
2017-01-01 22:35:25 +0000

    Adapt model to new checking routine

--- src/sso/dasso/models.py
+++ src/sso/dasso/models.py
@@ -7,6 +7,13 @@ class Ressource(models.Model):
     def __str__(self):
         return self.vhost
 
+    def matches(self, request):
+        return request.split('/')[0] == self.vhost
+
+    @property
+    def priority(self):
+        return 1
+
     class Meta:
         permissions = (
             ('access_ressource', 'Access ressource'),
