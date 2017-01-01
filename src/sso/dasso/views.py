978c2165f1979be4ec2f7aa94dc3d3e94bf50b8b
DaLynX <d@l.ynx.fr>
2017-01-01 22:33:52 +0000

    Check access based on full request in HTTP headers

--- src/sso/dasso/views.py
+++ src/sso/dasso/views.py
@@ -4,8 +4,17 @@ from django.http import HttpResponse, HttpResponseRedirect
 from .models import Ressource
 
 
-def check_user_access(request, target_vhost):
-    target_ressource = Ressource.objects.get(vhost=target_vhost)
+
+def check_user_access(request):
+    if ('HTTP_X_ORIGINAL_HOST' in request.META and 'HTTP_X_ORIGINAL_URI' in request.META):
+        requested_ressource = request.META['HTTP_X_ORIGINAL_HOST'] + request.META['HTTP_X_ORIGINAL_URI']
+    else:
+        return HttpResponse(status=500)
+    target_ressource = None
+    for r in Ressource.objects.all():
+        if r.matches(requested_ressource):
+            if not target_ressource or target_ressource.priority < r.priority:
+                target_ressource = r
     if request.user.has_perm('access_ressource', target_ressource):
         return HttpResponse()
     elif request.user.is_anonymous:
