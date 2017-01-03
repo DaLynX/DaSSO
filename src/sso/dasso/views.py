11cd407914bb01371b62f89a1df1d7af0d86ffd0
DaLynX <d@l.ynx.fr>
2017-01-03 15:50:40 +0000

    Added logging for easier debugging

--- src/sso/dasso/views.py
+++ src/sso/dasso/views.py
@@ -3,11 +3,14 @@ from django.http import HttpResponse, HttpResponseRedirect
 
 from .models import Ressource
 
+import logging
 
+logger = logging.getLogger('django.server')
 
 def check_user_access(request):
     if ('HTTP_X_ORIGINAL_HOST' in request.META and 'HTTP_X_ORIGINAL_URI' in request.META):
         requested_ressource = request.META['HTTP_X_ORIGINAL_HOST'] + request.META['HTTP_X_ORIGINAL_URI']
+        logger.info("Access requested to: [{}]".format(requested_ressource))
     else:
         return HttpResponse(status=500)
     target_ressource = None
@@ -15,7 +18,9 @@ def check_user_access(request):
         if r.matches(requested_ressource):
             if not target_ressource or target_ressource.priority < r.priority:
                 target_ressource = r
+                logger.info("Selected matching ressource: [{}]".format(r))
     if request.user.has_perm('access_ressource', target_ressource):
+        logger.info("Access granted to [{}] as per ressource [{}].".format(requested_ressource, target_ressource))
         return HttpResponse()
     elif request.user.is_anonymous:
         return HttpResponse(status=401)
